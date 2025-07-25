170b52743f6e93e6e0380686c0f286b50500d93d
Vineeth Alisyam <valisyam@shivelybros.com>
2025-07-25 17:13:10 -0400

    Added lines before app.listen in contact-api.js

--- src/components/contact-api.js
+++ src/components/contact-api.js
@@ -46,4 +46,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
